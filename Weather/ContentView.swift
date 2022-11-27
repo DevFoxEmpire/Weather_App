d14fea66e76c21c01c530d41be5c6d270e62d1e5
OlliGN <Olga199411@gmail.com>
2022-11-27 12:22:10 -0600

    Initial Commit

---
+++ Weather/ContentView.swift
@@ -0,0 +1,87 @@
+//
+//  ContentView.swift
+//  Weather
+//
+//  Created by Olha Hladush on 11/27/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.blue, .white]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                .edgesIgnoringSafeArea(.all)
+            
+            //MARK: main VStack
+            VStack {
+                Text("Cupportino, CA")
+                    .font(.system(size: 32, weight: .medium))
+                    .padding()
+                
+                //MARK: VStack for main icon and temperature
+                VStack(spacing: 8) {
+                    Image(systemName: "cloud.sun.fill")
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 180, height: 180)
+                    
+                    Text("76°")
+                        .font(.system(size: 70, weight: .medium))
+                    
+                }
+                Spacer()
+                
+                //MARK: HStack for weater by day
+                HStack(spacing: 20) {
+                    
+                    WeatherDayView(dayOfWeek: "TUE",
+                                   imageName: "cloud.sun.fill",
+                                   temperature: 76)
+                    WeatherDayView(dayOfWeek: "WED",
+                                   imageName: "sun.max.fill",
+                                   temperature: 70)
+                    WeatherDayView(dayOfWeek: "THU",
+                                   imageName: "wind",
+                                   temperature: 66)
+                    WeatherDayView(dayOfWeek: "FRI",
+                                   imageName: "sunset.fill",
+                                   temperature: 60)
+                    WeatherDayView(dayOfWeek: "SAT",
+                                   imageName: "moon.stars.fill",
+                                   temperature: 55)
+                }
+                Spacer()
+                
+            }.foregroundColor(.white)
+        }
+    }
+}
+
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
+    
+    struct WeatherDayView: View {
+        var dayOfWeek:String
+        var imageName:String
+        var temperature:Int
+        
+        var body: some View {
+            VStack(spacing: 10){
+                Text(dayOfWeek)
+                    .font(.system(size: 16, weight: .medium))
+                Image(systemName: imageName)
+                    .renderingMode(.original)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 40, height: 40)
+                Text(String("\(temperature)°"))
+                    .font(.system(size: 28, weight: .medium))
+            }
+        }
+    }
+
---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Olha Hladush on 11/27/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
