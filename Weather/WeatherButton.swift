9bb9519691d0c1240ac282ac2775162b3ccebf35
OlliGN <Olga199411@gmail.com>
2022-11-27 13:30:46 -0600

    Create separate file for button

---
+++ Weather/WeatherButton.swift
@@ -0,0 +1,22 @@
+//
+//  WeatherButton.swift
+//  Weather
+//
+//  Created by Olha Hladush on 11/27/22.
+//
+
+import SwiftUI
+
+struct WeatherButton: View {
+    var title:String
+    var textColor: Color
+    var backgroundColor: Color
+    var body: some View {
+        Text(title)
+            .frame(width: 280, height: 50)
+            .background(backgroundColor)
+            .foregroundColor(textColor)
+            .font(.system(size: 20, weight: .bold, design: .default))
+            .cornerRadius(10)
+    }
+}
